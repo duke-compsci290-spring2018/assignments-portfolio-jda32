a378ec51cc0d7d8bcd2671f5b70f6a38398eec6b
jasakers <jda32@duke.edu>
2018-04-05 01:47:19 -0400

    Pretty much finished with Quiz. Need to add one more quiz and CSS. Also fix gitignore.

--- Quiz/src/App.vue
+++ Quiz/src/App.vue
@@ -6,6 +6,7 @@
             <button @click="choseMathQuiz()">Math</button>
             <button @click="choseDukeQuiz()">Duke</button>
         </ul>
+        <h3 v-if="prevQuiz">Previous Quiz: {{prevQuiz.title}} </br> Score: {{prevQuiz.correct}}  out of {{prevQuiz.tries}}</h3>
     </div>
     <div id="quiz" v-if="quizChosen">
             <dukeQuiz
@@ -13,13 +14,16 @@
                 :theQuiz="currentQuiz"
                 :allResponses="responses"
                 :chosen="currentResponse"
-                :completedQuestions="completedQuestions"
-                v-on:chosen="addCurrentNumber">
+                :completedQuests="completedQuestions"
+                :nextQuest="addCurrentNumber"
+                :prevQuest="prevNumber"
+                :selectQuest="choseQuestion"
+                :submit="submitAnswer"
+                :small="smallest">
             </dukeQuiz>
-        <button v-if="currentQuestion > 0" @click="prevNumber()">Prev Question</button>
-        <button v-if="currentQuestion + 1 < currentQuiz.questions.length" @click="addCurrentNumber()">Next Question</button>
-        <button v-if="currentQuestion + 1 === currentQuiz.questions.length" @click="finishQuiz()">Finish Quiz</button>
-        <button @click="returnSelection()">Return To Selection Screen</button>
+        <!-- <button v-if="currentQuestion > 0" @click="prevNumber()">Prev Question</button> -->
+        <!-- <button v-if="currentQuestion + 1 < currentQuiz.questions.length" @click="addCurrentNumber()">Next Question</button> -->
+        <button v-if="completedQuestions.length+1 === currentQuiz.questions.length" @click="finishQuiz()">Finish Quiz</button>
     </div>
   </div>
 </template>
@@ -36,14 +40,20 @@ export default {
       msg: 'Select a quiz to take:',
       myMath: math,
       myDuke: duke,
+      quizes: [math,duke],
       selectQuiz: true,
       quizChosen: false,
       currentQuiz: '',
+      prevQuiz: '',
       currentQuizMsg: '',
       currentQuestion:0,
       responses: [],
       currentResponse: '',
-      completedQuestions: []
+      completedQuestions: [],
+      remainingQuestions: [],
+      tries: 0,
+      correct: 0,
+      smallest: 0
     }
   },
   components:{
@@ -56,6 +66,11 @@ export default {
         this.quizChosen=true
         this.currentQuizMsg=this.myMath.title
         this.currentQuiz=this.myMath
+        for(var i = 0; i < this.currentQuiz.questions.length; i++){
+            this.remainingQuestions.push(i)
+            console.log(i)
+        }
+        console.log(this.remainingQuestions)
         console.log(this.currentQuiz)
     },
     choseDukeQuiz(){
@@ -63,28 +78,97 @@ export default {
         this.quizChosen=true
         this.currentQuizMsg=this.myDuke.title
         this.currentQuiz=this.myDuke
+        for(var i = 0; i < this.currentQuiz.questions.length; i++){
+            this.remainingQuestions.push(i)
+            console.log(i)
+        }
         console.log(this.currentQuiz.questions[0].question)    
     },
     returnSelection(){
         this.quizChosen=false
         this.selectQuiz=true
     },
-    addCurrentNumber(e){
-        if(this.currentResponse !== null){
-            this.currentQuiz.questions[this.currentQuestion].completed = true
-            this.responses[this.currentQuestion] = this.currentResponse
-            this.currentResponse = ''
+    addCurrentNumber: function(e){
             this.currentQuestion = this.currentQuestion+1
-            console.log("Responses" + this.responses)
-            console.log("currentResponse" + this.currentResponse)
-        }
-        else{
-            return false
-        }
-        
+            while(this.completedQuestions.includes(this.currentQuestion)){
+                this.currentQuestion = this.currentQuestion+1
+            } 
     },
     prevNumber(){
-        this.currentQuestion = this.currentQuestion-1
+        var dummy = this.currentQuestion
+        console.log("Dummy:" + dummy)
+        dummy = dummy -1
+        while(dummy >= 0){
+            if(this.remainingQuestions.includes(dummy)){
+                this.currentQuestion = dummy
+                break
+            }
+            console.log(dummy)
+            console.log(this.remainingQuestions)
+            dummy = dummy - 1
+        }
+    },
+    choseQuestion(e){
+        this.currentQuestion = e
+    },
+    submitAnswer(e){
+        console.log(this.currentQuiz.questions[this.currentQuestion].correctAnswer)
+        console.log("E: " + e)
+        if(this.currentQuiz.questions[this.currentQuestion].completed === true){
+            alert("Already Answered Question")
+        }
+        else{
+            if(this.currentQuiz.questions[this.currentQuestion].correctAnswer === this.currentQuiz.questions[this.currentQuestion].choices.indexOf(e)){
+                alert("CORRECT")
+                this.correct++
+                this.tries++
+                this.currentQuiz.questions[this.currentQuestion].completed=true
+                this.completedQuestions.push(this.currentQuestion)
+                var index = this.remainingQuestions.indexOf(this.currentQuestion)
+                if(index > -1){
+                        this.remainingQuestions.splice(index, 1)
+                    }
+                while(this.completedQuestions.includes(this.currentQuestion) && this.completedQuestions.length < this.currentQuiz.questions.length){
+                    if(this.currentQuestion === this.currentQuiz.questions.length-1){
+                        this.currentQuestion = 0
+                     }
+                     else{
+                        this.currentQuestion = this.currentQuestion+1
+                     }
+                    
+                }
+                if(this.remainingQuestions.length > 0){
+                    var temp = this.remainingQuestions[0]
+                    for(var i = 0; i<this.remainingQuestions.length;i++){
+                        if(this.remainingQuestions[i] < temp){
+                            temp = this.remainingQuestions[i]
+                        }
+                    }
+                    this.smallest = temp
+                }
+                if(this.remainingQuestions.length === 0){
+                    console.log("Finished")
+                    this.currentQuiz.tries = this.tries
+                    this.currentQuiz.correct = this.correct
+                    this.currentQuiz.finished = true
+                    this.prevQuiz= this.currentQuiz
+                    this.completedQuestions = []
+                    this.quizChosen=false
+                    this.selectQuiz=true
+                    for(var i=0; i < this.currentQuiz.questions.length; i++){
+                        this.currentQuiz.questions[i].completed = false
+                    }
+                    this.currentQuiz=''
+                    this.currentQuestion=0
+                    this.correct = 0
+                    this.tries = 0
+                }
+            }
+            else{
+                alert("WRONG")
+                this.tries++
+            }
+        }
     },
     finishQuiz(){
         this.currentQuestion = 0
--- Quiz/src/Components/dukeQuiz.vue
+++ Quiz/src/Components/dukeQuiz.vue
@@ -7,23 +7,26 @@
             <input type="radio" id="'chosen'+key" :value="choice" v-model="chosen"><label :for="'chosen'+key">{{choice}}</label><br/>
         </div>
         <p v-if="chosen">This is your response: {{chosen}}</p>
+        <button v-if="chosen && !theQuiz.questions[currentNumber].completed" @click="submit(chosen)">Submit Answer</button>
     </div>
-    <select v-model="currentNumber">
-        <option v-for="(question,key) in theQuiz.questions" v-if="!question.completed" v-bind:value="key">
-            {{key+1}}
-        </option>
-    </select>
+    <button v-if="currentNumber!==0 && completedQuests.length+1!==theQuiz.questions.length && currentNumber-small!==0" @click="prevQuest">Prev Question</button>
+    <button v-if="completedQuests.length+1!==theQuiz.questions.length" @click="nextQuest(chosen)">Next Question</button>
+    <div id="select">
+        <select v-model="currentNumber" @change="selectQuest(currentNumber)">
+            <option v-for="(question,key) in theQuiz.questions" v-if="!question.completed" v-bind:value="key">
+                {{key+1}}
+            </option>
+        </select>
+    </div>
 </div>
 </template>
 
+
+
 <script>
+
 // export anonymous object from this module so it can be accessed by others when imported
 export default {
-    data(){
-        return{
-            chosen:''
-        }
-    },
     name: 'App',
     // these values will be filled in by parent component, values must match tag's attribute names
     props:[
@@ -31,10 +34,15 @@ export default {
         'currentNumber',
         'chosen',
         'allResponses',
-        'completedQuestions'
-    ],
-    
+        'completedQuests',
+        'nextQuest',
+        'prevQuest',
+        'selectQuest',
+        'submit',
+        'small'
+    ]
 }
+
 </script>
 
 
