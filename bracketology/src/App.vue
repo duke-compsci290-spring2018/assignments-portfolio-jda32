d9055ff40f84e18838907fde59711a61d922baf8
jasakers <jda32@duke.edu>
2018-04-28 12:17:49 -0400

    FLEXBOXES AND D3

--- bracketology/src/App.vue
+++ bracketology/src/App.vue
@@ -1,17 +1,76 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
-    <router-view/>
+    <h1>{{ msg }}</h1>
+    <h2>Essential Links</h2>
+    <ul>
+      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
+      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
+      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
+      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
+    </ul>
+    <div id="minimal" v-on:click="sample($event)">
+        <button>Yeet!</button>
+    </div>
+    <div id="bracket"></div>
+    <h2>Ecosystem</h2>
+    <ul>
+      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
+      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
+      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
+      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
+    </ul>
+    <div class="flex-container">
+        <div>1</div>
+        <div>2</div>
+        <div>3</div>
+    </div>
+
   </div>
 </template>
 
+
 <script>
+var firebase = require('firebase')
+// Load expose-loader such that I can use jQuery within vue
+console.log()
+var config = {
+    apiKey: "AIzaSyCVzFAh--3WNTrUmR21C-TsxdhUdBp6PIk",
+    authDomain: "bracketology-dd459.firebaseapp.com",
+    databaseURL: "https://bracketology-dd459.firebaseio.com",
+    projectId: "bracketology-dd459",
+    storageBucket: "bracketology-dd459.appspot.com",
+    messagingSenderId: "48478220972"
+  }
+var db = firebase.initializeApp(config).database()
 export default {
-  name: 'App'
+  name: 'app',
+  data () {
+    return {
+        msg: 'Welcome to Your Vue.js App',
+        minimalData: {
+            teams : [
+            ["Team 1", "Team 2"], /* first matchup */
+            ["Team 3", "Team 4"]  /* second matchup */
+            ],
+            results : [
+            [[1,2], [3,4]],       /* first round */
+            [[4,6], [2,1]]        /* second round */
+            ]
+        }
+    }
+  },
+  methods: {
+    sample: function(event){
+        
+        d3.select("body").style("background-color", "yellow").transition().delay(1000).styleTween("background-color", function(){return d3.interpolate("yellow", "cyan")}).duration(5000)
+        
+    }
+  }
 }
 </script>
 
-<style>
+<style lang="scss">
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -19,5 +78,35 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+.flex-container {
+  display: flex;
+  background-color: DodgerBlue;
+  flex-direction: column;
+}
+
+.flex-container > div {
+  background-color: #f1f1f1;
+  margin: 10px;
+  padding: 20px;
+  font-size: 30px;
 }
 </style>
--- bracketology/src/main.js
+++ bracketology/src/main.js
@@ -1,15 +1,71 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
-import router from './router'
 
-Vue.config.productionTip = false
+import VueFire from 'VueFire'
+import App from './App.vue'
+import * as d3 from 'd3'
+import Flexbox from 'flexbox-vue'
+Vue.use(Flexbox)
 
-/* eslint-disable no-new */
+import 'expose-loader?$!expose-loader?jQuery!jquery'
+
+Vue.use(VueFire)
+
 new Vue({
-  el: '#app',
-  router,
-  components: { App },
-  template: '<App/>'
+    el: '#app',
+    components:{
+        App
+    },
+    template: '<App/>',
+    render: h => h(App)
 })
+
+var height = 750,
+    width = 1000,
+    curve_width = 20,
+    entry_width = (width - 10*curve_width)/11;
+
+var svg = d3.select("div").append("svg")
+                        .attr("height", height)
+                        .attr("width", width);
+
+var rounds = [32, 16, 8, 4, 2, 1]
+
+// Start drawing left side
+
+var i;
+
+rounds.forEach( function (d, j) {
+  var entry_height = height/d;
+  if (j > 0) {
+    for (i = 0; i < rounds[j-1]; i++) {
+      var x = (entry_width)*j + (j-1)*curve_width;
+      var y = (i + 0.5)*(entry_height/2);
+      var a = i % 2 == 0 ? 0.25:-0.25;
+        svg.append("path").attr("d", "M" + x + " " + y + "c10,0 10," + a*entry_height + " 20," + a*entry_height).attr("stroke", "black").attr("stroke-width", 3);
+    }
+  }
+  for (i = 0; i < d; i++){
+    var x = (entry_width + curve_width)*j;
+    var y = (i + 0.5)*entry_height;
+    svg.append("path").attr("d", "M" + x + " " + y + " l " + entry_width + " 0").attr("stroke", "black").attr("stroke-width", 3);
+  }
+});
+
+var right = svg.append("g").attr("transform", "translate(" + width + ",0) scale(-1,1)")
+
+rounds.forEach( function (d, j) {
+  var entry_height = height/d;
+  if (j > 0) {
+    for (i = 0; i < rounds[j-1]; i++) {
+      var x = (entry_width)*j + (j-1)*curve_width;
+      var y = (i + 0.5)*(entry_height/2);
+      var a = i % 2 == 0 ? 0.25:-0.25;
+        right.append("path").attr("d", "M" + x + " " + y + "c10,0 10," + a*entry_height + " 20," + a*entry_height).attr("stroke", "black").attr("stroke-width", 3);
+    }
+  }
+  for (i = 0; i < d; i++){
+    var x = (entry_width + curve_width)*j;
+    var y = (i + 0.5)*entry_height;
+    right.append("path").attr("d", "M" + x + " " + y + " l " + entry_width + " 0").attr("stroke", "black").attr("stroke-width", 3);
+  }
+});
